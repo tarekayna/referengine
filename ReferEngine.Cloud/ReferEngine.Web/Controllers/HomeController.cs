5fede0e13390a2a6d681e9e6373d5786772f95ba
Tarek Ayna <tarek@apexa.co>
2013-02-05 10:38:35 -0800

    Remove Home Variations

--- ReferEngine.Cloud/ReferEngine.Web/Controllers/HomeController.cs
+++ ReferEngine.Cloud/ReferEngine.Web/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Web.Mvc;
 using ReferEngine.Common.Models;
 using ReferEngine.Common.Utilities;
@@ -28,121 +27,7 @@ namespace ReferEngine.Web.Controllers
         [HttpGet]
         public ActionResult Index()
         {
-            LandingPageVariation landingPageToUse = GetLandingPage();
-            ViewBag.LandingPageName = landingPageToUse.Name;
-            SetViewProperties(landingPageToUse.ViewProperties);
-
             return View();
-        }
-
-        private void SetViewProperties(ViewProperties viewProperties)
-        {
-            ViewBag.ButtonClass = viewProperties.ButtonClass;
-            ViewBag.HeadlineText = viewProperties.HeadlineText;
-            ViewBag.FocusOnInput = viewProperties.FocusOnInput;
-        }
-
-        private static LandingPageVariation GetLandingPage()
-        {
-            List<LandingPageVariation> list = new List<LandingPageVariation>
-                                                  {
-                                                      new LandingPageVariation 
-                                                          {
-                                                              Name = "Base",
-                                                              Probability = 20,
-                                                              ViewProperties = new ViewProperties()
-                                                          },
-                                                      new LandingPageVariation 
-                                                          {
-                                                              Name = "WinWinFirst",
-                                                              Probability = 20,
-                                                              ViewProperties = new ViewProperties
-                                                                                   {
-                                                                                       HeadlineText = "It's a win-win! Increase your app sales and reward your customers."
-                                                                                   }
-                                                          },
-                                                      new LandingPageVariation 
-                                                          {
-                                                              Name = "GoViral",
-                                                              Probability = 25,
-                                                              ViewProperties = new ViewProperties
-                                                                                   {
-                                                                                       HeadlineText = "Your app can go viral with Refer Engine. Increase your app sales and reward your customers."
-                                                                                   }
-                                                          },
-                                                      new LandingPageVariation 
-                                                          {
-                                                              Name = "GoViralOrange",
-                                                              Probability = 25,
-                                                              ViewProperties = new ViewProperties
-                                                                                   {
-                                                                                       HeadlineText = "Your app can go viral with Refer Engine. Increase your app sales and reward your customers.",
-                                                                                       ButtonClass = "btn-warning"
-                                                                                   }
-                                                          },
-                                                      new LandingPageVariation
-                                                          {
-                                                              Name = "OrangeButton",
-                                                              Probability = 10,
-                                                              ViewProperties = new ViewProperties
-                                                                                   {
-                                                                                       ButtonClass = "btn-warning"
-                                                                                   }
-                                                          },
-                                                      new LandingPageVariation
-                                                          {
-                                                              Name = "RedButton",
-                                                              Probability = 0,
-                                                              ViewProperties = new ViewProperties
-                                                                                   {
-                                                                                       ButtonClass = "btn-danger"
-                                                                                   }
-                                                          },
-                                                      new LandingPageVariation
-                                                          {
-                                                              Name = "GreenButton",
-                                                              Probability = 0,
-                                                              ViewProperties = new ViewProperties
-                                                                                   {
-                                                                                       ButtonClass = "btn-success"
-                                                                                   }
-                                                          }
-                                                  };
-
-            List<LandingPageVariation> weightedList = new List<LandingPageVariation>();
-            foreach (LandingPageVariation landingPage in list)
-            {
-                for (int j = 0; j < landingPage.Probability; j++)
-                {
-                    weightedList.Add(landingPage);
-                }
-            }
-            Random random = new Random((int)DateTime.Now.Ticks);
-
-            LandingPageVariation landingPageToUse = false ? list[0] : weightedList[random.Next(0, weightedList.Count - 1)];
-
-            return landingPageToUse;
-        }
-
-        private class ViewProperties
-        {
-            public string ButtonClass { get; set; }
-            public string HeadlineText { get; set; }
-            public bool FocusOnInput { get; set; }
-
-            internal ViewProperties()
-            {
-                FocusOnInput = false;
-                HeadlineText = "Increase your app sales and reward your customers. It's a win-win!";
-                ButtonClass = "btn-primary";
-            }
-        }
-
-        private class LandingPageVariation
-        {
-            public string Name { get; set; }
-            public int Probability { get; set; }
-            public ViewProperties ViewProperties { get; set; }
         }
     }
 }
