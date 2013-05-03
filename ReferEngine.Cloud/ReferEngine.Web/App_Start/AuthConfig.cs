75497e1130ec860ed00f3fc5c2bfa111d1763594
Tarek Ayna <tarek@apexa.co>
2013-05-02 18:50:44 -0700

    Facebook Sdk 1

---
+++ ReferEngine.Cloud/ReferEngine.Web/App_Start/AuthConfig.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Web.WebPages.OAuth;
+
+namespace ReferEngine.Web.App_Start
+{
+    public static class AuthConfig
+    {
+        public static void RegisterAuth()
+        {
+            //OAuthWebSecurity.RegisterMicrosoftClient(
+            //    clientId: "",
+            //    clientSecret: "");
+
+            //OAuthWebSecurity.RegisterTwitterClient(
+            //    consumerKey: "",
+            //    consumerSecret: "");
+
+            OAuthWebSecurity.RegisterFacebookClient("368842109866922", "b673f45aa978225ae8c9e4817a726be7");
+
+            //OAuthWebSecurity.RegisterGoogleClient();
+        }
+    }
+}
--- ReferEngine.Cloud/ReferEngine.Web/Global.asax.cs
+++ ReferEngine.Cloud/ReferEngine.Web/Global.asax.cs
@@ -19,7 +19,7 @@ namespace ReferEngine.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-
+            AuthConfig.RegisterAuth();
             ServiceBusOperations.Initialize();
         }
     }
